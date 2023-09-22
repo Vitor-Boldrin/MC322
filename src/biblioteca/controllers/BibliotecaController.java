ab0d52b44df7546736aef33f73b4974a37a3536a
VitorBoldrinCL <vitorboldrin@avato.com.br>
2023-09-22 15:42:12 -0300

    Update BibliotecaController.java

--- src/biblioteca/controllers/BibliotecaController.java
+++ src/biblioteca/controllers/BibliotecaController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import biblioteca.models.ItemMultimidia.ItemMultimidia;
 import biblioteca.models.Membro.Membro;
 
-public interface BibliotecaController<T> {
+public interface BibliotecaController {
     List<ItemMultimidia> consultarItensDisponiveis();
     ItemMultimidia buscarItenPorIdentificacao(int identificacao);
 }
